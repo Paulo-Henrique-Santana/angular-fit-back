faebfe8350e14dac31fb22c02c7ee79e8c1532d2
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2024-09-03 00:04:41 -0300

    fix: permitir requisição do navegador

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,19 @@ import routes from './routes';
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use(routes);
